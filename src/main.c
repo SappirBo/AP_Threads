02b0217a59df0ecef09ce9ab6898f5e36bc42195
SappirBo <sappirbohbot@gmail.com>
2023-04-29 19:12:57 +0300

    add bigFile

--- src/main.c
+++ src/main.c
@@ -3,11 +3,11 @@
 #include <string.h>
 #include <stdlib.h>
 
-
+# define MAX_SIZE 1024
 
 int main(int argc, char *argv[])
 {
-	if (argc != 2)
+	if (argc != 5)
 	{
 	    printf("usage: key < file \n");
 	    printf("!! data more than 1024 char will be ignored !!\n");
@@ -15,11 +15,38 @@ int main(int argc, char *argv[])
 	}
 
 	int key = atoi(argv[1]);
-	printf("key is %i \n",key);
+    char flag[MAX_SIZE];
+    char source_file[MAX_SIZE];
+    char dest_file[MAX_SIZE];
+    strcpy(flag, argv[2]);
+    strcpy(source_file,argv[3]);
+    strcpy(dest_file,argv[4]);
+	
+    
+    printf("key is %i,",key);
+    if(!strcmp(flag,"-d"))
+    {
+        printf("Flag = decrypt\n");    
+    }
+    else if(!strcmp(flag,"-e"))
+    {
+        printf("Flag = encrypt\n");   
+    }
+    else
+    {
+        printf("Flag is UNKNOWN.\n");
+        return 0;
+    }
+
+    printf("Source: %s, Dest: %s\n",source_file,dest_file);
+
+
+
+
 
 	char c;
 	int counter = 0;
-	int dest_size = 1024;
+	int dest_size = MAX_SIZE;
 	char data[dest_size]; 
 	
 
